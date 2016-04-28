9c5a4d8817b78d2ec703b2e1e9f9cc88d168c270
Alexander Harchenko <morontt@gmail.com>
2016-04-29 00:30:58 +0300

    remove unused vendors

--- src/main/java/conf/Routes.java
+++ src/main/java/conf/Routes.java
@@ -25,18 +25,15 @@ import controllers.ApplicationController;
 public class Routes implements ApplicationRoutes {
 
     @Override
-    public void init(Router router) {  
-        
+    public void init(Router router) {
         router.GET().route("/").with(ApplicationController.class, "index");
         router.GET().route("/hello_world.json").with(ApplicationController.class, "helloWorldJson");
-        
- 
+
         ///////////////////////////////////////////////////////////////////////
         // Assets (pictures / javascript)
-        ///////////////////////////////////////////////////////////////////////    
-        router.GET().route("/assets/webjars/{fileName: .*}").with(AssetsController.class, "serveWebJars");
+        ///////////////////////////////////////////////////////////////////////
         router.GET().route("/assets/{fileName: .*}").with(AssetsController.class, "serveStatic");
-        
+
         ///////////////////////////////////////////////////////////////////////
         // Index / Catchall shows index page
         ///////////////////////////////////////////////////////////////////////
